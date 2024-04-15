0f2ac5204be728aaa8eef0653d50edcb2c0edd14
VinzR-2159109 <vinz.roosen@student.uhasselt.be>
2024-04-15 11:51:30 +0200

    Add mqqt_connector

---
+++ Webots/Project/controllers/robot_controller/Modules/mqtt_connector.py
@@ -0,0 +1,28 @@
+import paho.mqtt.client as paho
+import json
+
+class MQTTConnector():
+    BROKER = "19dec47c4ea94f028a9fb739c51578c2.s1.eu.hivemq.cloud"
+    PORT = 8883
+    
+    def __init__(self):
+        self.client = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION2)
+        self.client.tls_set(tls_version=paho.ssl.PROTOCOL_TLS)
+        self.client.username_pw_set("Robot", "Robot123")
+
+        self.client.connect(
+            host=self.BROKER,
+            port=self.PORT,
+            clean_start=paho.MQTT_CLEAN_START_FIRST_ONLY,
+            keepalive=60
+        )
+
+        self.client.loop_start()
+
+    def publish(self, topic, data):
+        payload = json.dumps(data)
+        self.client.publish(topic, payload)
+
+    def disconnect(self):
+        self.client.disconnect()
+        self.client.loop_stop()
